5a489ef6282dd5674bbc00fe7ad93374dfa3a25c
LeoKnox <LeoKnox@users.noreply.github.com>
2021-02-02 22:10:27 -0800

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -9,7 +9,7 @@ namespace consoleapp
             Console.WriteLine("\nWrite some text! ");
             var text = Console.ReadLine();
             var date = DateTime.Now;
-            Console.WriteLine($"\nYou wrote {text} in the consle.", text);
+            Console.WriteLine($"\nYou wrote {text} in the consle.");
         }
     }
 }
