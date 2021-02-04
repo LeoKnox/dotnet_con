25e95fa6ba8f7fa6d2e21d2b211313f953e22cf3
LeoKnox <LeoKnox@users.noreply.github.com>
2021-02-03 22:19:56 -0800

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -9,7 +9,7 @@ namespace consoleapp
             Console.WriteLine("\nWrite some text! ");
             var text = Console.ReadLine();
             var date = DateTime.Now;
-            Console.WriteLine($"\nYou wrote {text} in the consle.");
+            Console.WriteLine(text == "text" ? "Unoriginal write something else" : $"\nYou wrote {text} in the consle.");
             string literal_string = @"This is a path: c:\Windows\System32";
             Console.WriteLine(literal_string);
         }
