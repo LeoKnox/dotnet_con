56377af0948450df96e67720181ab9c986efb04b
LeoKnox <LeoKnox@users.noreply.github.com>
2021-02-07 22:19:36 -0800

    Add files via upload

---
+++ EF_Activity01/EF_Activity01/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace EF_Activity01
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+            // class library for use later.
+        }
+    }
+}
