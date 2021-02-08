37babb1bc8c7385d2f0e99b9dd9fb3cf987c259b
LeoKnox <LeoKnox@users.noreply.github.com>
2021-02-07 22:32:35 -0800

    Add files via upload

---
+++ EF_Activity02/EF_Activity02/Class1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Data.Entity;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EF_Activity01_NetFramework
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(string connectionString) : base(connectionString)
+        {
+
+        }
+    }
+}
