c88e74d62eb34ff6f6b76859a378219c06623f74
LeoKnox <LeoKnox@users.noreply.github.com>
2021-02-08 22:08:16 -0800

    Create Class02.cs

---
+++ Class02.cs
@@ -0,0 +1,22 @@
+using System.Linq;
+using System;
+
+public class Program
+{
+	class char_Class
+	{
+		public int CharID { get; set; }
+		public String Name { get; set; }
+		public int Level { get; set; }
+		public int Atk { get; set; }
+		public int HP { get; set; }
+	}
+	public static void Main()
+	{
+		char_Class[] partyArray = { 
+            new char_Class() { CharID = 1, Name = "Aelien", Level = 18, Atk = 17, HP = 34 },
+			new char_Class() { CharID = 2, Name = "Eveehi", Level = 18, Atk = 23, HP = 28 },
+			new char_Class() { CharID = 3, Name = "Ynzon", Level = 18, Atk = 12, HP = 54 }
+        };
+	}
+}
